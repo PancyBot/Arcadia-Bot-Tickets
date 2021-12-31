a1ba35170e7b7f9ee914a0712f5e2de966491637
FrancisCortes <franciscortes50@gmail.com>
2021-12-31 14:24:38 -0600

    Cambios finales

--- events/Client/ready.js
+++ events/Client/ready.js
@@ -1,1 +1,20 @@
 const { Client } = require('discord.js')
+
+module.exports = {
+    name: 'ready',
+    once: true,
+
+    /**
+     * 
+     * @param {Client} client 
+     */
+
+    async execute(client) {
+        client.user.setPresence({
+            activities: [{ name: `Usuarios: ${client.users.cache.size.toLocaleString()} || v0.9.8 Beta Oficial`, type: 'WATCHING' }],
+            status: 'dnd',
+        })
+
+        console.log(`El bot ${client.user.tag} esta listo, Usuarios en cache: ${client.users.cache.size}`)
+    } 
+}
